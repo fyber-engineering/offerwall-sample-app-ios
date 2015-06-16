17b23f26fcdabf830ee438cc807e206342d2d7a5
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2015-06-16 21:28:11 +0200

    offer wall simple usage

--- FyberSampleApp/Classes/AppDelegate.m
+++ FyberSampleApp/Classes/AppDelegate.m
@@ -5,7 +5,6 @@
 //
 
 #import "AppDelegate.h"
-
 #import "FyberSDK.h"
 
 
--- FyberSampleApp/Classes/Controllers/OfferWallViewController.m
+++ FyberSampleApp/Classes/Controllers/OfferWallViewController.m
@@ -5,6 +5,7 @@
 //
 
 #import "OfferWallViewController.h"
+#import "FyberSDK.h"
 
 
 @interface OfferWallViewController ()
@@ -34,6 +35,21 @@
 - (IBAction)showOfferWall:(id)sender
 {
     NSLog(@"Showing Offer Wall");
+    // Create an instance of the FYBOfferWallViewController
+    FYBOfferWallViewController *offerWallViewController = [[FYBOfferWallViewController alloc] init];
+    
+    // You can give the user the possibility to close the Offer Wall while it's loading
+    // offerWallViewController.showCloseButtonOnLoad = YES;
+    
+    // Show the Offer Wall
+    [offerWallViewController presentFromViewController:self animated:YES completion:^{
+        // Code executed when the Offer Wall is presented
+        NSLog(@"Offer was presented");
+    } dismiss:^{
+        // Code executed when the Offer Wall is dismissed
+        NSLog(@"Offer is dismissed");
+    }];
+
 }
 
 @end
