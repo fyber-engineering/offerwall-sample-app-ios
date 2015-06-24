960fb6c5050e84dbb1c79bf6d82031de3cd3c9da
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2015-06-24 16:30:09 +0200

    set corder radius in layout

--- FyberSampleApp/Classes/Views/FYBButton.m
+++ FyberSampleApp/Classes/Views/FYBButton.m
@@ -32,10 +32,18 @@
 - (void)customizeAppearance
 {
     self.layer.borderWidth = 2.0f;
-    self.layer.cornerRadius = 100.0f;
     self.layer.borderColor = [UIColor fyb_textColor].CGColor;
     self.backgroundColor = [UIColor fyb_brownColor];
     self.titleLabel.font = [UIFont fyb_buttonFont];
+}
+
+#pragma mark - View Lifecycle
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    self.layer.cornerRadius = CGRectGetWidth(self.frame) / 2.0f;
+
 }
 
 
