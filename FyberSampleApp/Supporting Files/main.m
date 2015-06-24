4f5fd3af23be16d8415e7e34ebcc302ded80cd16
Titouan Van Belle <titouan.vanbelle@orange.fr>
2015-06-24 10:40:35 +0200

    Copyright + Header Search Paths // No Trace of tito

--- FyberSampleApp/Supporting Files/main.m
+++ FyberSampleApp/Supporting Files/main.m
@@ -1,9 +1,7 @@
 //
-//  main.m
-//  FyberSampleApp
 //
-//  Created by tito on 12/06/15.
-//  Copyright (c) 2015 Fyber. All rights reserved.
+// Copyright (c) 2015 Fyber. All rights reserved.
+//
 //
 
 #import <UIKit/UIKit.h>
